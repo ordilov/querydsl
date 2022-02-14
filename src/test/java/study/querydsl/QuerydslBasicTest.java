3d555c69216cbb21f60c0c0dfebf16732cb6ea4c
ordilov <ordilov@gmail.com>
2022-02-14 19:27:00 +0900

    SQL function 호출하기

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -667,4 +667,27 @@ public class QuerydslBasicTest {
         .execute();
   }
 
+  @Test
+  public void sqlFunction(){
+    List<String> result = queryFactory
+        .select(Expressions.stringTemplate("function('replace', {0}, {1}, {2})",
+            member.username, "member", "M"))
+        .from(member)
+        .fetch();
+
+    result.forEach(System.out::println);
+  }
+
+  @Test
+  public void sqlFunction2(){
+    List<String> result = queryFactory
+        .select(member.username)
+        .from(member)
+//        .where(member.username.eq(
+//            Expressions.stringTemplate("function('lower', {0})", member.username)))
+        .where(member.username.eq(member.username.lower()))
+        .fetch();
+
+    result.forEach(System.out::println);
+  }
 }
