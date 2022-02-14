046664ee816c5f8798c58ed7ba1f3124a0f8f771
ordilov <ordilov@gmail.com>
2022-02-14 17:48:59 +0900

    프로젝션과 결과 반환 - 기본

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -454,4 +454,29 @@ public class QuerydslBasicTest {
     result.forEach(System.out::println);
   }
 
+  @Test
+  public void simpleProjection(){
+    List<String> result = queryFactory
+        .select(member.username)
+        .from(member)
+        .fetch();
+
+    result.forEach(System.out::println);
+  }
+
+  @Test
+  public void tupleProjection(){
+    List<Tuple> result = queryFactory
+        .select(member.username, member.age)
+        .from(member)
+        .fetch();
+
+    result.forEach(t -> {
+      String username = t.get(member.username);
+      Integer age = t.get(member.age);
+      System.out.println("username = " + username);
+      System.out.println("age = " + age);
+    });
+  }
+
 }
