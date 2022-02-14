817ae2ec1e616ac95539afab6410491eaf5b5310
ordilov <ordilov@gmail.com>
2022-02-14 19:19:38 +0900

    수정, 삭제 벌크 연산

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -25,6 +25,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
 import study.querydsl.dto.QMemberDto;
@@ -631,4 +632,39 @@ public class QuerydslBasicTest {
   private BooleanExpression allEq(String usernameCond, Integer ageCond) {
     return usernameEq(usernameCond).and(ageEq(ageCond));
   }
+
+  @Test
+  public void bulkUpdate() {
+    long count = queryFactory
+        .update(member)
+        .set(member.username, "비회원")
+        .where(member.age.lt(28))
+        .execute();
+
+    em.flush();
+    em.clear();
+
+    List<Member> result = queryFactory
+        .selectFrom(member)
+        .fetch();
+
+    result.forEach(System.out::println);
+  }
+
+  @Test
+  public void bulkAdd() {
+    long count = queryFactory
+        .update(member)
+        .set(member.age, member.age.add(1))
+        .execute();
+  }
+
+  @Test
+  public void bulkDelete() {
+    long count = queryFactory
+        .delete(member)
+        .where(member.age.gt(18))
+        .execute();
+  }
+
 }
